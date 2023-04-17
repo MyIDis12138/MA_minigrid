d5c0e414ebe7cb990624838f1de5b432b7acc11f
yang <yanggu123138@gmail.com>
2023-04-17 17:47:05 +0100

    upload test.py

--- test.py
+++ test.py
@@ -4,42 +4,65 @@ from MA_minigrid.ManualControl import ManualControl
 from MA_minigrid.wrappers import SingleAgentWrapper
 from MA_minigrid.envs.MAbabyai.query_wrapper import MultiGrid_Safety_Query
 
-from tqdm import tqdm
-import time
-
-def get_human_action(num):
-    action_map = [
-        "left",
-        "right",
-        "forward",
-        "pickup",
-        "drop",
-        "toggle",
-        "done",
-    ]
-
-    return [action_map[num]]
+key_to_action = {
+    "a": [["left"]],
+    "d": [["right"]],
+    "w": [["forward"]],
+    " ": [["toggle"]],
+    "pageup": [["pickup"]],
+    "pagedown": [["drop"]],
+    "tab": [["pickup"]],
+    "left shift": [["drop"]],
+    "enter": [["done"]],
+}
 
 
-def main():
-    env = gym.make('SQbabyai-DangerGround-v0')
-    #env = gym.make('SQbabyai-DangerRoom-v0')
-    #env = gym.make('SQbabyai-DangerAgent-v0')
-    env = SingleAgentWrapper(env)
-    #env = MultiGrid_Safety_Query(env)
+questions_ground = [
+            ["what", "is", "danger", "ground"], 
+            ["what", "is", "danger", "zone"],
+            ["what", "is", "danger", "area"],
+            ["where", "is", "danger", "ground"], 
+            ["where", "is", "danger", "zone"],
+            ["where", "is", "danger", "area"],
+            ["where", "is", "danger", "robot"],
+]
 
-    env.reset()
-    
-    #while running:
-    for _ in tqdm(range(1000000)):
-        env.render()
-        time.sleep(0.1)
+questions_room = [
+            ["what", "is", "jack", "room"],
+            ["what", "is", "mary", "room"],
+            ["what", "is", "tom", "room"],
+            ["what", "is", "mike", "room"],
+            ["where", "is", "jack", "room"],
+            ["where", "is", "mary", "room"],
+            ["where", "is", "tom", "room"],
+            ["where", "is", "mike", "room"],
+            ["what", "is", "danger", "room"],
+            ["where", "is", "danger", "room"],
+            ["what", "is", "jack", "toy"],
+            ["what", "is", "mary", "toy"],
+            ["what", "is", "tom", "toy"],
+            ["what", "is", "mike", "toy"],
+]
 
-        a = env.action_space.sample()
-        obs, reward, done, info = env.step(a)
-        if done:
-            env.reset()
-    
+questions_agent =[
+            ["what", "is", "danger", "robot"],
+            ["where", "is", "danger", "robot"],
+]
+
+
+map = {
+    'SQbabyai-DangerGround-v0': questions_ground,
+    'SQbabyai-DangerRoom-v0': questions_room,
+    'SQbabyai-DangerAgent-v0': questions_agent,
+}
+
 
 if __name__ == "__main__":
-    main()
+    #env_name = 'SQbabyai-DangerGround-v0'
+    #env_name = 'SQbabyai-DangerRoom-v0'
+    env_name = 'SQbabyai-DangerAgent-v0'
+    env = gym.make(env_name)
+    env = SingleAgentWrapper(env)
+    env = MultiGrid_Safety_Query(env, verbose=True)
+    manual_control = ManualControl(env, key_to_action=key_to_action, question_set=map[env_name])
+    manual_control.start()
